f228752d3735d6692159bce05b0cedc79688705a
Jean Paul Baladi <81441213+jeanbaladi@users.noreply.github.com>
2022-06-12 14:44:21 -0300

    Update js.js

--- js.js
+++ js.js
@@ -14,7 +14,7 @@ const arr = [];
 let listItem = document.createElement("li");
 /*creo mi primera funcion llamada a travez del evento "click"
 La tarea de esta funcion es verificar si hay texto agregado en el input y si el elemento a agregar ya existe, 
-en caso que el elemento a agregar ya exista retornara una funcion distinta a que si el elemento no existiriea*/
+en caso que el elemento a agregar ya exista retornara una funcion distinta a que si el elemento no existiriera*/
 btn.addEventListener("click" , () => {
     /*llamo a un funcion cuyo objetivo es verificar si el elemento a agregar ya existe en el array*/
     let find = findElement(addList.value);
@@ -122,7 +122,8 @@ function suppress(textElement)
 /* Mi funcion favorita de todo el script */
 function findElement(textElement)
 {
-    /* mediante el meotod findIndex busco algun valor que coincida con el parametro de mi funcion, este metodo me devuelve el indice del objeto que coincida*/
+    /* mediante el meotod findIndex busco algun valor que coincida con el parametro de mi funcion, este metodo me devuelve el indice del objeto que coincida y
+    en caso qie no coincida devuelve -1*/
     return arr.findIndex((item) => item.value == textElement);
 }
 /* Funcion destinada a enviar un mensaje al usuario notificando que la tarea que quiere agregar ya existe */
